1286ae822200406d9fedffe2d3d73df279afee3c
Serdar MSC <serdarmsc@gmail.com>
2016-01-29 19:51:50 +0200

    Update user_config.h

--- include/user_config.h
+++ include/user_config.h
@@ -1,7 +1,7 @@
 #ifndef __USER_CONFIG_H__
 #define __USER_CONFIG_H__
 
-#define BAUD 38400
+#define BAUD 115200
 
 #define AP_SSID		"uart_tcp_bridge"
 #define AP_PASSWORD	"BridgeTest"
@@ -9,7 +9,7 @@
 #define TX_POWER 81
 #define CHANNEL 6
 
-#define TCP_PORT 8000
+#define TCP_PORT 9999
 
 /*
  * if KEEP_CLIENT defined:  connection will be kept open until client disconnects by itself
